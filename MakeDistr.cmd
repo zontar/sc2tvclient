a16533cbadd0600da7ea28608d72bb3904930100
Vladimir <vladimir@zontar.ru>
2014-04-28 01:12:35 +0700

    Добавил недостающие элементы в дистриб

--- MakeDistr.cmd
+++ MakeDistr.cmd
@@ -17,16 +17,24 @@ goto end
 :CopyRelease
 mkdir %DestDir%\Release
 mkdir %DestDir%\Release\platforms
+mkdir %DestDir%\Release\imageformats
+mkdir %DestDir%\Release\QtQuick.2
 for %%F in (%ReleaseFiles%) do robocopy %1\bin %DestDir%\Release %%F /MIR
 robocopy %SrcDir%\plugins\platforms %DestDir%\Release\platforms qwindows.dll /MIR
+robocopy %SrcDir%\imageformats\platforms %DestDir%\Release\imageformats qjpeg.dll /MIR
+robocopy %SrcDir%\qml\QtQuick.2 %DestDir%\Release\QtQuick.2 /MIR /xf qtquick2plugind.dll
 copy /y %OpenSslDir%\*.dll %DestDir%\Release
 goto end
 
 :CopyDebug
 mkdir %DestDir%\Debug
 mkdir %DestDir%\Debug\platforms
+mkdir %DestDir%\Debug\imageformats
+mkdir %DestDir%\Debug\QtQuick.2
 for %%F in (%DebugFiles%) do robocopy %1\bin %DestDir%\Debug %%F /MIR
 robocopy %SrcDir%\plugins\platforms %DestDir%\Debug\platforms qwindowsd.dll /MIR
+robocopy %SrcDir%\imageformats\platforms %DestDir%\Debug\imageformats qjpegd.dll /MIR
+robocopy %SrcDir%\qml\QtQuick.2 %DestDir%\Debug\QtQuick.2 /MIR /xf qtquick2plugin.dll
 copy /y %OpenSslDir%\*.dll %DestDir%\Debug
 goto end
 
